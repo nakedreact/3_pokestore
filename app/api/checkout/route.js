ad68bb57f0f94edd5ea3484bdbbf93cb70b25e86
Sebastian Cardenas Valdez <sebastiancv4000@gmail.com>
2025-06-07 18:55:12 -0600

    Fonts

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,8 +1,8 @@
 import Stripe from "stripe";
 import "../../../envConfig.js";
 
-const API_KEY = process.env.STRIPE_SECRET_KEY;
-const stripe = new Stripe(API_KEY);
+const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
+const stripe = new Stripe(API_KEY, { apiVersion: "2025-05-28.basil" });
 
 export async function POST(request) {
   try {
